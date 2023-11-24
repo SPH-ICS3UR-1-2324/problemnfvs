dc1ed08fdffd47016327bdd20ad62cfe8bca810e
mikiele12 <144146911+mikiele12@users.noreply.github.com>
2023-11-24 11:09:31 -0500

    a little changes

--- src/problemnfvs/Main2.java
+++ src/problemnfvs/Main2.java
@@ -18,12 +18,29 @@ public class Main2 {
 			System.out.println("3: Quit");
 			
 			System.out.println("Enter your choice:");
-			Scanner scanner = null;
-			int choice = scanner.nextInt();
 			
+			int choice = in.nextInt();
+		
 			switch (choice) {
 			case 1: 
+				System.out.println("Enter a number");
+				int userInput = in.nextInt();
+				if(userInput>=0) {
+					max=Math.max(max, userInput);
+					min=Math.min(min,userInput);
+					Object userIput = in;
+					Object sum = userIput;
+					int count = 0;
+					count++;
+				}else {
+					System.out.println("Negative number entered. Exiting program");
+					return;
+				}
+				break;
+			case 2:
 			}
+			 int input = in.nextInt();
+				
 		}
 	}
 
